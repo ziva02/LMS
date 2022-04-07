2be702ad654149cff62f27d6b57f249ad7bd675d
tian0900 <82864213+tian0900@users.noreply.github.com>
2022-04-07 19:31:01 +0700

    halo

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -179,7 +179,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="../../index3.html" class="brand-link">
+    <a href="{{url('tabeldenahsatulantaidua ')}}" class="brand-link">
       <img src="{{asset('dist')}}/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Canteen IT Del</span>
     </a>
