9ed8d4121e90564dddd9231ec306b57e38f9d150
tian0900 <82864213+tian0900@users.noreply.github.com>
2022-06-26 09:46:01 +0700

    asda

--- resources/views/kantinduasatu.blade.php
+++ resources/views/kantinduasatu.blade.php
@@ -34,11 +34,11 @@
                 <div class="mt-2">
                   <div class="row">
                     <div class="col-md-6">  
-                    <h6 class="text-left">{{$tampil -> nama}} </h6>     
+                    <h7 class="text-left">{{$tampil -> nama}} </h7>     
                     </div>
                   <div class="col-md-6">
 
-                    <h6 class="text-right">{{$tampil -> namadua}}</h6>
+                    <h7 class="text-right">{{$tampil -> namadua}}</h7>
                     </div>
 
                   </div>
